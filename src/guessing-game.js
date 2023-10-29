8d89db425d33eccac758070cd183733d14843231
Alexandra Dementeva <topchiy.alexandra@gmail.com>
2023-10-29 22:25:26 +0300

    1

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.guessValue = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.guessValue = Math.floor((this.min + this.max) / 2);
+        return this.guessValue;
     }
 
     lower() {
-
+        this.max = this.guessValue - 1;
     }
 
     greater() {
-
+        this.min = this.guessValue + 1;
     }
 }
+
 
 module.exports = GuessingGame;
